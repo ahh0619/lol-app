acabb1f8d8488f4a26dc303f43c22ed5e0d6585a
ahh0619 <ahh900619@gmail.com>
2024-12-17 10:47:02 +0900

    add:홈 화면 비디오배경

--- src/app/(info)/page.tsx
+++ src/app/(info)/page.tsx
@@ -1,9 +1,11 @@
+import VideoBackground from "@/components/layout/VideoBackground";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <section className="grid justify-items-center min-h-screen py-8 pb-20 m-auto max-w-custom container">
+      <VideoBackground />
       <article className="flex flex-col gap-10 p-4">
         <div className="txt">
           <h2 className="text-3xl font-bold">리그 오브 레전드 정보 앱</h2>
@@ -11,7 +13,6 @@ export default function Home() {
             Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.
           </p>
         </div>
-
         <div className="link">
           <ul className="flex flex-col gap-3 items-center justify-center lg:flex-row ">
             <li>
